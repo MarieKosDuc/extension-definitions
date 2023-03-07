cf494d48d71e0839ffbe51982e145163e40bf95a
Marie Koscianski-Ducharlet <115166678+MarieKosDuc@users.noreply.github.com>
2023-03-07 17:49:19 +0100

    First version of extension

---
+++ script.js
@@ -0,0 +1,30 @@
+// Setting variables
+let apiUrl = "https://projet-api-sigma.vercel.app/definition";
+let button = document.getElementById("get-new");
+
+console.log("ok");
+
+// Fetch API to display a tech definition
+const getDefinition = () => {
+fetch(apiUrl)
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function (value) {
+    // produit
+    document.getElementById("title").innerHTML = value.title; // affiche l'intitulé
+    document.getElementById("definition").innerHTML = value.description; // affiche la définition
+  })
+  .catch(function (err) {});
+};
+
+// Display a first definition on page opening
+getDefinition()
+
+// Display a new definition on button click
+button.onclick = () => {
+  console.log("clic!");
+  getDefinition();
+};
