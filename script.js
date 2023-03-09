d62885797cc5ddbc93f686af9b5174ddd2d2a2ce
Marie Koscianski-Ducharlet <115166678+MarieKosDuc@users.noreply.github.com>
2023-03-09 17:41:43 +0100

    Fixed CSS!

--- script.js
+++ script.js
@@ -1,28 +1,134 @@
 // Setting variables
-let apiUrl = "https://projet-api-mariekosduc.vercel.app/definition";
-let button = document.getElementById("get-new-definition");
+const apiUrl = "https://projet-api-mariekosduc.vercel.app";
+const buttonRandom = document.getElementById("get-new-definition");
+const buttonParadigmes = document.getElementById("get-paradigmes");
+const buttonLangages = document.getElementById("get-langages");
+const buttonArchitecture = document.getElementById("get-architecture");
+const buttonSystemes = document.getElementById("get-systemes-reseau");
+const buttonStructures = document.getElementById("get-structures-exec");
 
-// Fetch API to display a tech definition
+let categorySwitch = "/random";
+
+// ------------ Display a random definition -----------------
+
+// get a random definition from API, all categories mixed
 const getDefinition = () => {
-fetch(apiUrl)
-  .then(function (res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function (value) {
-    // produit
-    document.getElementById("title").innerHTML = value.title + " :"; // affiche l'intitulé
-    document.getElementById("definition").innerHTML = value.description; // affiche la définition
-  })
-  .catch(function (err) {});
+  fetch(apiUrl + categorySwitch)
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (value) {
+      document.getElementById("title").innerHTML = value.title + " :";
+      document.getElementById("definition").innerHTML = value.description;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 };
 
-// Display a first definition on page opening
-getDefinition()
+// Display a first random definition on page opening
+getDefinition();
 
 // Display a new definition on button click
-button.onclick = () => {
-  console.log("clic!");
+buttonRandom.onclick = () => {
+  categorySwitch = "/random";
   getDefinition();
 };
+
+buttonParadigmes.onclick = () => {
+  categorySwitch = "/paradigmes/random";
+  getDefinition();
+};
+
+buttonLangages.onclick = () => {
+  categorySwitch = "/langages/random";
+  getDefinition();
+};
+
+buttonArchitecture.onclick = () => {
+  categorySwitch = "/architecture/random";
+  getDefinition();
+};
+
+buttonSystemes.onclick = () => {
+  categorySwitch = "/systeme-reseau/random";
+  getDefinition();
+};
+
+buttonStructures.onclick = () => {
+  categorySwitch = "/structures-exec/random";
+  getDefinition();
+};
+
+// ---------------- Add choices to the select tags by category ---------------
+
+const paradigmesArray = [];
+const getTitlesParadigmes = [paradigmesArray, "/paradigmes"];
+const paradigmesSelect = document.getElementById("select-paradigmes");
+
+const langagesArray = [];
+const getTitlesLangages = [langagesArray, "/langages"];
+
+const architectureArray = [];
+const getTitlesArchitecture = [architectureArray, "/architecture"];
+
+const systemesArray = [];
+const getTitlesSystemes = [systemesArray, "/systeme-reseau"];
+
+const testArray = [];
+const getTitlesTest = [testArray, "/culture-test"];
+
+const structuresArray = [];
+const getTitlesStructures = [structuresArray, "/structures-exec"];
+
+// get all titles from a category and push them in an array
+function getAllTitles(catArray, catSelect) {
+  fetch(apiUrl + catArray[1])
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (value) {
+      for (let i = 0; i < value.length; i++) {
+        catArray[0].push(value[i].title);
+      }
+      catArray = catArray[0];
+      return catArray;
+    })
+    .then(function (catArray, catSelect) {
+      setParadigmesTitle(catArray, catSelect);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+getAllTitles(getTitlesParadigmes);
+getAllTitles(getTitlesLangages);
+getAllTitles(getTitlesArchitecture);
+getAllTitles(getTitlesSystemes);
+getAllTitles(getTitlesTest);
+getAllTitles(getTitlesStructures);
+
+// ESSAI
+// let anOption = new Option("Texte", "Valeur", false, false);
+// paradigmesSelect.add(anOption);
+
+// let selectValue = paradigmesSelect.value;
+// paradigmesSelect.onchange = () => {
+//   console.log(selectValue);
+// };
+
+// const paradigmesSelect = document.getElementById("select-paradigmes");
+
+// pushing all titles to the select tag
+function setParadigmesTitle(array, select) {
+  for (let i = 0; i < array.length; i++) {
+    console.log("helo");
+    let option = new Option(array[i], array[i], false, false);
+    select.add(option);
+  }
+}
