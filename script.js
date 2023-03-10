b72dbbbc6ff3a6b0b5cc21b172d8a141e1f157cf
Marie Koscianski-Ducharlet <115166678+MarieKosDuc@users.noreply.github.com>
2023-03-10 16:52:21 +0100

    Full working versiongit add .

--- script.js
+++ script.js
@@ -70,27 +70,35 @@ const paradigmesSelect = document.getElementById("select-paradigmes");
 
 const langagesArray = [];
 const getTitlesLangages = [langagesArray, "/langages"];
+const langagesSelect = document.getElementById("select-langages");
 
 const architectureArray = [];
 const getTitlesArchitecture = [architectureArray, "/architecture"];
+const architectureSelect = document.getElementById("select-architecture");
 
 const systemesArray = [];
 const getTitlesSystemes = [systemesArray, "/systeme-reseau"];
+const systemesSelect = document.getElementById("select-systemes");
 
 const testArray = [];
 const getTitlesTest = [testArray, "/culture-test"];
+const testCultureSelect = document.getElementById("select-culture-test");
 
 const structuresArray = [];
 const getTitlesStructures = [structuresArray, "/structures-exec"];
+const structuresSelect = document.getElementById("select-structures-exec");
+
 
 // get all titles from a category and push them in an array
 function getAllTitles(catArray, catSelect) {
+  // fetch API on category endpoint (as set in catArray[1])
   fetch(apiUrl + catArray[1])
     .then(function (res) {
       if (res.ok) {
         return res.json();
       }
     })
+    // push definitions titles into catArray[0], reduce catArray to its [0] index to eliminate the category endpoint
     .then(function (value) {
       for (let i = 0; i < value.length; i++) {
         catArray[0].push(value[i].title);
@@ -98,7 +106,8 @@ function getAllTitles(catArray, catSelect) {
       catArray = catArray[0];
       return catArray;
     })
-    .then(function (catArray, catSelect) {
+    // use catArray to create HTML options in the category's "select" tag
+    .then(function (catArray) {
       setParadigmesTitle(catArray, catSelect);
     })
     .catch(function (err) {
@@ -106,29 +115,75 @@ function getAllTitles(catArray, catSelect) {
     });
 }
 
-getAllTitles(getTitlesParadigmes);
-getAllTitles(getTitlesLangages);
-getAllTitles(getTitlesArchitecture);
-getAllTitles(getTitlesSystemes);
-getAllTitles(getTitlesTest);
-getAllTitles(getTitlesStructures);
-
-// ESSAI
-// let anOption = new Option("Texte", "Valeur", false, false);
-// paradigmesSelect.add(anOption);
-
-// let selectValue = paradigmesSelect.value;
-// paradigmesSelect.onchange = () => {
-//   console.log(selectValue);
-// };
-
-// const paradigmesSelect = document.getElementById("select-paradigmes");
-
 // pushing all titles to the select tag
 function setParadigmesTitle(array, select) {
   for (let i = 0; i < array.length; i++) {
-    console.log("helo");
-    let option = new Option(array[i], array[i], false, false);
+    // for each definition: create an "option" HTML tag with title as text, index as value
+    let option = new Option(array[i], i, false, false);
     select.add(option);
   }
 }
+
+getAllTitles(getTitlesParadigmes, paradigmesSelect);
+getAllTitles(getTitlesLangages, langagesSelect);
+getAllTitles(getTitlesArchitecture, architectureSelect);
+getAllTitles(getTitlesSystemes, systemesSelect);
+getAllTitles(getTitlesTest, testCultureSelect);
+getAllTitles(getTitlesStructures, structuresSelect);
+
+// Display the selected definition in main div
+
+paradigmesSelect.onchange = () => {
+  let selectValue = paradigmesSelect.value;
+  console.log(selectValue);
+  if (selectValue){
+    categorySwitch = `/paradigmes/${selectValue}`;
+    getDefinition();
+  }
+};
+
+langagesSelect.onchange = () => {
+  let selectValue = langagesSelect.value;
+  console.log(selectValue);
+  if (selectValue){
+    categorySwitch = `/langages/${selectValue}`;
+    getDefinition();
+  }
+};
+
+architectureSelect.onchange = () => {
+  let selectValue = architectureSelect.value;
+  console.log(selectValue);
+  if (selectValue){
+    categorySwitch = `/langages/${selectValue}`;
+    getDefinition();
+  }
+};
+
+systemesSelect.onchange = () => {
+  let selectValue = systemesSelect.value;
+  console.log(selectValue);
+  if (selectValue){
+    categorySwitch = `/systemes-reseau/${selectValue}`;
+    getDefinition();
+  }
+};
+
+testCultureSelect.onchange = () => {
+  let selectValue = testCultureSelect.value;
+  console.log(selectValue);
+  if (selectValue){
+    categorySwitch = `/culture-test/${selectValue}`;
+    getDefinition();
+  }
+};
+
+structuresSelect.onchange = () => {
+  let selectValue = structuresSelect.value;
+  console.log(selectValue);
+  if (selectValue){
+    categorySwitch = `/structures-exec/${selectValue}`;
+    getDefinition();
+  }
+};
+
